2902166f0535d8905e91132e3a4de6944b2b9b63
Qasim Muhammad <qasim@Qasims-iMac.local>
2025-08-08 21:33:47 +0100

    bug fixed

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,28 +1,28 @@
-import React from 'react'
+// import React from 'react'
 
-const About = () => {
-  return (
-    <div name='about' className='w-full h-screen bg-[#0a192f] text-gray-300'>
-        <div className='flex flex-col justify-center items-center w-full h-full'>
-        <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
-            <div className='sm:text-right pb-8 pl-4'>
-                <p className='text-4xl font-bold inline border-b-4 border-red-500'>About</p>
-            </div>  
-            <div></div>
-        </div>  
-            <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
-                <div className='sm:text-right text-4xl font-bold'>
-                <p> Hi. I am Qasim, nice to meet you, please take a look around.</p>
-                </div>
+// const About = () => {
+//   return (
+//     <div name='about' className='w-full h-screen bg-[#0a192f] text-gray-300'>
+//         <div className='flex flex-col justify-center items-center w-full h-full'>
+//         <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
+//             <div className='sm:text-right pb-8 pl-4'>
+//                 <p className='text-4xl font-bold inline border-b-4 border-red-500'>About</p>
+//             </div>  
+//             <div></div>
+//         </div>  
+//             <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
+//                 <div className='sm:text-right text-4xl font-bold'>
+//                 <p> Hi. I am Qasim, nice to meet you, please take a look around.</p>
+//                 </div>
             
-            <div>
-                <p> I am passionate, excellent developer that improves lives of those around me. I am specialise in creating websites for clients ranging from individuals, small-business all the way to large enterprise corporations. What would you do if you had a web developer expert available at your fingertips?</p>
-            </div>      
-        </div>
-        </div>  
+//             <div>
+//                 <p> I am passionate, excellent developer that improves lives of those around me. I am specialise in creating websites for clients ranging from individuals, small-business all the way to large enterprise corporations. What would you do if you had a web developer expert available at your fingertips?</p>
+//             </div>      
+//         </div>
+//         </div>  
       
-    </div>
-  )
-}
+//     </div>
+//   )
+// }
 
-export default About
+// export default About
