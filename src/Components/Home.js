f4cd8367eb2d8f58e5b786ee686e71a3b48d1ced
Qasim Muhammad <qasim@Qasims-iMac.local>
2025-08-08 14:44:28 +0100

    Updated portfolio with new projects and improved layout

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -11,7 +11,7 @@ const Home = () => {
     <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
     <p className='text-red-500'> Hi, my name is </p>
     <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Qasim Muhammad</h1>
-    <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a Frontend Developer</h2>
+    <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a Frontend web developer</h2>
     <p className='text-[#8892b0] py-4 max-w-[700px]'> I’m a Frontend Developer and UI/UX Designer with 15 years of experience in the design industry, having worked with leading tech giants from Silicon Valley to craft user-focused, visually compelling digital products. </p>
     <div>
     
